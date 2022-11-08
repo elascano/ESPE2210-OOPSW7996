604dfbe4d9f436981143d7fd1bc8901aa0e1fbb6
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2022-11-08 09:43:28 -0500

    update ws10 file

--- ws/trejo/u1/ws10/HelloWorld.java
+++ ws/trejo/u1/ws10/HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main(String[] args){
-			System.out.println(" Hello Object Oriented Programming Students");
+			System.out.println(" Hello Object Oriented Programming classmates");
 			System.out.println(" ----- My name is Alex Trejo----");
 			System.out.println("The best time of the day is now");
 	}
