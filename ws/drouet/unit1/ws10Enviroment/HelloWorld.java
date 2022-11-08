c48fe4e246f6f843bcca22d65968efca1a62474a
Stephen David Drouet Navarrete <sddrouet@espe.edu.ec>
2022-11-08 09:24:12 -0500

    ws10Enviroment

---
+++ ws/drouet/unit1/ws10Enviroment/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello Object Orientef Programming Students");
+        System.out.println(" ------- My name is Stephen Drouet ----------");
+        System.out.println("This is a code example of Java");
+    }
+}
