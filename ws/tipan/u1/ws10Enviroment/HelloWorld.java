cebb92b9c305a5a001db079de31639500f25e4c4
Reishel Dayelin Tipán Ávila <rdtipan1@espe.edu.ec>
2022-11-08 15:17:49 -0500

    V1.0 ws10Enviroment

---
+++ ws/tipan/u1/ws10Enviroment/HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld{
+       public static void main(String[] args){
+                System.out.println(" Hello Object Oriented Programming Students");
+                System.out.println(" -------- My name is Reishel Tipan --------");
+                System.out.println("This is going ro be a fantastic semeter----");
+       }
+}
