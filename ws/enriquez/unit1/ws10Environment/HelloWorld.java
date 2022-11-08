5c10c92acad08f5fa2c11f10fedd050fe76caa85
Sheylee Arielle Enriquez Hernandez <sheylee.ariel@hotmail.com>
2022-11-08 11:41:12 -0500

    ws10Environment

---
+++ ws/enriquez/unit1/ws10Environment/HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+        public static void main(String[] args){
+                System.out.println(" Hello Edison Lascano");
+                System.out.println(" -------- My name is Sheylee Enriquez --------");
+                System.out.println(" This is going to be a fantastic semester :)");
+         }
+}
+
