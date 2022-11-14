c5c98638742ccef10e44341a8f75be40d158ec12
LeonardoYaranga <leonardo.yaranga@gmail.com>
2022-11-14 17:17:56 -0500

    hw04ImputFromKeyboard V1.0.0

--- ws/yaranga/u1/ws11FromUmlToCode/ChickenFarmS/src/ec/edu/espe/chickenfarm/view/ChickenFarmSimulator.java
+++ ws/yaranga/u1/ws11FromUmlToCode/ChickenFarmS/src/ec/edu/espe/chickenfarm/view/ChickenFarmSimulator.java
@@ -11,26 +11,20 @@ import java.util.Scanner;
 public class ChickenFarmSimulator {
     public static void main(String[] args) {
         System.out.println("Leonardo Yaranga");
-        System.out.println("setters and getters");
+        System.out.println("ImputFromkeyboard");
         System.out.println("====================");
         
-        //int numbersOfFeathers;  //datatype variable
         
         //declaration
-       /* Chicken chicken; //ADT variableName
+        Chicken chicken; //ADT variableName
         
-        //numbersOfFeathers = 7;
-                                    //Bad method to get data. *Bad practice*
-        //System.out.println("The number of feathers is -->" + numbersOfFeathers);
-        
-        //CorrectMethodToGetData
        //initializing the chicken
        //creating the instance
         chicken = new Chicken();
         
-        System.out.println("chicken -->" + chicken);
+        //System.out.println("chicken -->" + chicken);
         
-        chicken.setId(432);
+       /* chicken.setId(432);
         chicken.setName("Lucy");
         chicken.setAge(4);
         chicken.setColor("White and Brown");
@@ -42,7 +36,7 @@ public class ChickenFarmSimulator {
         System.out.println("chicken age \t-->" + chicken.getAge());
         System.out.println("chicken color \t--> " + chicken.getColor());
         System.out.println("chicken is molting \t--> " + chicken.isIsMolting());
-        
+        */
         
         //read from the keywoard the information for a new chicken
         //declare the variables for imput
@@ -50,11 +44,31 @@ public class ChickenFarmSimulator {
         //create a new chicken
         //set the values for the chicken with the data that was entered
         //print the information
-        */
-       Scanner chicken = new Scanner(System.in);
-       chicken.id = chicken.nextInt();
-               
-               
+        
+       Scanner imput = new Scanner(System.in);
+        System.out.println("What is the id?");
+        chicken.setId(imput.nextInt());
+        imput.nextLine();
+        
+        System.out.println("What is the name?");
+ 
+       chicken.setName(imput.nextLine());
+       
+        System.out.println("What is the age?");
+        chicken.setAge(imput.nextInt());
+        imput.nextLine();
+        
+        System.out.println("What is the color?");
+        chicken.setColor(imput.nextLine());
+        
+        System.out.println("Is molting?");
+        chicken.setIsMolting(imput.nextBoolean());
+        System.out.println("====================");  
+        System.out.println("chicken id \t --> " + chicken.getId());
+        System.out.println("chicken name \t--> " + chicken.getName());
+        System.out.println("chicken age \t-->" + chicken.getAge());
+        System.out.println("chicken color \t--> " + chicken.getColor());
+        System.out.println("chicken is molting \t--> " + chicken.isIsMolting());       
                
         
     }
