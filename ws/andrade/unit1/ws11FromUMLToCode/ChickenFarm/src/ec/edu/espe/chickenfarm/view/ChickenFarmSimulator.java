324c905c9ab7a661d0be2c76719793914f4232f5
Alejo Andrade <02alejo20@gmail.com>
2022-11-14 10:27:32 -0500

    Setters and getters

--- ws/andrade/unit1/ws11FromUMLToCode/ChickenFarm/src/ec/edu/espe/chickenfarm/view/ChickenFarmSimulator.java
+++ ws/andrade/unit1/ws11FromUMLToCode/ChickenFarm/src/ec/edu/espe/chickenfarm/view/ChickenFarmSimulator.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ec.edu.espe.chickenfarm.view;
+
+import ec.edu.espe.chickenfarm.model.Chicken;
+
+import ec.edu.espe.chickenfarm.model.Egg;
+
+
 
 /**
  *
  * @author Alejandro Andrade, Scriptal, DCCO_ESPE
  */
 public class ChickenFarmSimulator {
-    
+    public static void main(String[] args) {
+        
+        System.out.println("Alejandro Andrade");
+        System.out.println("Setters and getters");
+        System.out.println("===================");
+        
+        Chicken chicken;
+        //ADT
+        int numberOfFeathers = 5;
+        chicken = new Chicken();
+        
+        System.out.println("The number of feather is ---> " + numberOfFeathers);
+        
+        System.out.println("chicken ---> " + chicken);
+        
+        chicken.setId(432);
+        chicken.setName("Lucy");
+        chicken.setAge(32);
+        chicken.setColor("White and Brown");
+        chicken.setIsMolting(false);
+        
+        System.out.println("chicken id is ---> " + chicken.getId());
+        System.out.println("chicken name is ---> " + chicken.getName());
+        System.out.println("chicken age is ---> " + chicken.getAge());
+        System.out.println("chicken color is ---> " + chicken.getColor());
+        System.out.println("chicken is molting ---> " + chicken.isIsMolting());
+        
+    }
 }
