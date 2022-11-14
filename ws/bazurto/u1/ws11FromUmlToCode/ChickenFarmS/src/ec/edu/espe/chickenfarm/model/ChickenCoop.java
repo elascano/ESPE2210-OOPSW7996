833436a0dea38a66d003bc8394b98e40f2a2565e
cabazurto1 <bazurtomora@gmail.com>
2022-11-14 18:16:39 -0500

    Ws12 and Hw04 Bazurto

--- ws/bazurto/u1/ws11FromUmlToCode/ChickenFarmS/src/ec/edu/espe/chickenfarm/model/ChickenCoop.java
+++ ws/bazurto/u1/ws11FromUmlToCode/ChickenFarmS/src/ec/edu/espe/chickenfarm/model/ChickenCoop.java
@@ -1,4 +1,4 @@
-A/*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -8,6 +8,6 @@ package ec.edu.espe.chickenfarm.model;
  *
  * @author Christopher Bazurto, DeltaTeam, DCCO-ESPE
  */
-public class Chiken {
+public class ChickenCoop {
     
 }
