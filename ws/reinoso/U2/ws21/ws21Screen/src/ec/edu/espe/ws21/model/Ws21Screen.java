f1e0661bcc2477fe47d64630b69156ff04e626a5
JerlyReinoso <jerlyreinoso@gmail.com>
2022-12-09 01:13:30 -0500

    hw08v1.0.0

---
+++ ws/reinoso/U2/ws21/ws21Screen/src/ec/edu/espe/ws21/model/Ws21Screen.java
@@ -0,0 +1,37 @@
+package ec.edu.espe.ws21.model;
+
+/**
+ *
+ * @author Jerly Reinoso, Search Engine Bandits, DCCO-ESPE
+ */
+public class Ws21Screen {
+
+    private int id;
+    private String name;
+    private String price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+}
