462b1000ae61b2e8794cc5e92981fcd04981d67a
Reishel Dayelin Tipán Ávila <rdtipan1@espe.edu.ec>
2022-12-08 23:10:43 -0500

    V1.0 ws21 and hw08

--- hw/tipan/u2/hw08/hw08Chair/src/ec/edu/espe/hw08Chair/view/ArrayChair.java
+++ hw/tipan/u2/hw08/hw08Chair/src/ec/edu/espe/hw08Chair/view/ArrayChair.java
@@ -1,25 +1,25 @@
 
-package ec.edu.espe.ws21PairOfShoes.view;
+package ec.edu.espe.hw08Chair.view;
 
-import ec.edu.espe.ws21PairOfShoes.model.PairOfShoes;
-import static ec.edu.espe.ws21PairOfShoes.controller.FileManager.*;
+import ec.edu.espe.hw08Chair.model.Chair;
+import static ec.edu.espe.hw18Chair.controller.FileManager.*;
 import java.util.Scanner;
 import java.util.ArrayList;
 /**
  *
  * @author Reishel Tipan, PACSTORE, DCCO-ESPE
  */
-public class ArrayShoes {
+public class ArrayChair {
     public static void main(String[]args){
         
         Scanner keyboard=new Scanner(System.in);
-        ArrayList<PairOfShoes> shoesData=new ArrayList<>();
+        ArrayList<Chair> chairsData=new ArrayList<>();
         
         int choice;
-        String fileName="ShoesData.json";
+        String fileName="ChairsData.json";
         
         do{
-            shoesData=read(shoesData,"ShoesData.json");
+            chairsData=read(chairsData,"ChairsData.json");
             System.out.println("*******************");
             System.out.println("      WELCOME!     ");
             System.out.println("*******************");
@@ -27,28 +27,28 @@ public class ArrayShoes {
             System.out.println("  Reishel's System ");
             System.out.println("*******************");
             System.out.println("You want to do?");
-            System.out.println("1.Create a pair of shoes");
-            System.out.println("2.Show a pair of shoes");
-            System.out.println("3.Updade a pair of shoes");
-            System.out.println("4.Print a pair of shoes");
-            System.out.println("5.Delete a pair of shoes");
+            System.out.println("1.Create a chair");
+            System.out.println("2.Show a chair");
+            System.out.println("3.Updade a chair");
+            System.out.println("4.Print a chair");
+            System.out.println("5.Delete a chair");
             System.out.println("6.Exit");
             System.out.println("Choice-->");
             choice=keyboard.nextInt();
             
             switch(choice){
                 case 1->{
-                    addPairOfShoes(shoesData);
-                    create(shoesData,fileName);
-                    System.out.println("CREATED PAIR OF SHOES IN THE DATABASE!");
+                    addChair(chairsData);
+                    create(chairsData,fileName);
+                    System.out.println("CREATED CHAIR IN THE DATABASE!");
                 }
-                case 2->showPairOfShoes(shoesData);
+                case 2->showChair(chairsData);
                 
-                case 3->update(shoesData);
+                case 3->update(chairsData);
                 
-                case 4->printList(shoesData);
+                case 4->printList(chairsData);
                 
-                case 5->delete(shoesData);
+                case 5->delete(chairsData);
                 
                 case 6->System.out.println("THANKS FOR VISITING THE PROGRAM!!");
                 
@@ -59,16 +59,16 @@ public class ArrayShoes {
         }while(choice !=6);     
     }
     
-    private static void showPairOfShoes(ArrayList<PairOfShoes> shoesData){
+    private static void showChair(ArrayList<Chair> chairsData){
         int idLook;
         Scanner keyboard=new Scanner(System.in);
         try{
-            System.out.println("Enter ID the pair of shoes:");
+            System.out.println("Enter ID the chair:");
             idLook=keyboard.nextInt();
-            int shoesLocated=show(shoesData, idLook);
-            if(shoesLocated !=-1){
-                System.out.println("[ID]\t[Brand]\t[Size]");
-                printLine(shoesData.get(shoesLocated));
+            int chairsLocated=show(chairsData, idLook);
+            if(chairsLocated !=-1){
+                System.out.println("[ID]\t[Material]\t[Color]");
+                printLine(chairsData.get(chairsLocated));
             }else{
                 System.out.println("ID not Found");
             }
@@ -77,46 +77,47 @@ public class ArrayShoes {
         }
     }
     
-    public static void addPairOfShoes(ArrayList<PairOfShoes> shoes){
+    public static void addChair(ArrayList<Chair> chairs){
         Scanner keyboard=new Scanner(System.in);
-        PairOfShoes pairOfShoes;
-        int id,size;
-        String brand;
+        Chair chair;
+        int id;
+        String material,color;
         
         try{
             System.out.println("*****SUBMIT INFORMATION****");
             System.out.println("      Reishel's System     ");
-            System.out.println("Insert shoe ID:");
+            System.out.println("Insert chair ID:");
             id=keyboard.nextInt();
             while(checkId(id)){
                 System.out.println("ERROR! ID exists, insert other: ");
                 id=keyboard.nextInt();
             }
             keyboard.nextLine();
-            System.out.println("Insert shoe brand:");
-            brand=keyboard.nextLine();
-            System.out.println("Insert shoe size:");
-            size=keyboard.nextInt();
+            System.out.println("Insert chair material:");
+            material=keyboard.nextLine();
+            System.out.println("Insert chair color:");
+            color=keyboard.nextLine();
             
-            pairOfShoes=new PairOfShoes(id,brand,size);
+            chair=new Chair(id,material,color);
             System.out.println("********************");
-            shoes.add(pairOfShoes);
+            chairs.add(chair);
         }catch(Exception e){
             System.out.println("ERROR! Incorrect, insert other choice:");
         }
     }
     
     public static boolean checkId(int id){
-        ArrayList<PairOfShoes> shoes=new ArrayList<>();
-        shoes=read(shoes,"ShoesData.json");
-        PairOfShoes pairOfShoes=new PairOfShoes();
+        ArrayList<Chair> chairs=new ArrayList<>();
+        chairs=read(chairs,"ChairsData.json");
+        Chair chair=new Chair();
         
-        for(int i=0;i<shoes.size();i++){
-            pairOfShoes=shoes.get(i);
-            if(id==pairOfShoes.getId()){
+        for(int i=0;i<chairs.size();i++){
+            chair=chairs.get(i);
+            if(id==chair.getId()){
                 return true;
             }
         }
         return false;
-    }   
+    }
+    
 }
