a23e405b048c6a6749356b361f2ed6d6bdb14e3c
verdugong <sebastianvccv@gmail.com>
2022-12-12 23:09:43 -0500

    hw09 UserDefineExceptions V1.0.0

---
+++ exams/verdugo/u1/Excercise 28/src/excercise/pkg28/utils/nameException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excercise.pkg28.utils;
+
+/**
+ *
+ * @author Sebastian Verdugo, ProgressTeam, DCOO-ESPE
+ */
+public class nameException extends Exception{
+    public nameException(String message){
+        super(message);
+    }
+    
+}
