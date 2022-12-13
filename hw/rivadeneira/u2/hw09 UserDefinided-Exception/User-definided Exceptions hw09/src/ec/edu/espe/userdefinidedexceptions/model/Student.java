29372df0c6dab146530671f47f4e794420205049
darivadeneira <darivadeneira7@espe.edu.ec>
2022-12-12 21:00:48 -0500

    hw09 User Definided Exceptions v1.0.0

---
+++ hw/rivadeneira/u2/hw09 UserDefinided-Exception/User-definided Exceptions hw09/src/ec/edu/espe/userdefinidedexceptions/model/Student.java
@@ -0,0 +1,32 @@
+
+package ec.edu.espe.userdefinidedexceptions.model;
+
+
+/**
+ *
+ * @author Ariel Rivadeneira, Search Engine Bandits, DCCO-ESPE
+ */
+public class Student {
+
+    private int nrc;
+        
+    /**
+     * @return the nrc
+     */
+    public int getNrc() {
+        return nrc;
+    }
+
+    /**
+     * @param nrc the nrc to set
+     */
+    public void setNrc(int nrc) {
+        this.nrc = nrc;
+    }
+
+}
+    
+    
+ 
+    
+
