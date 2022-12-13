5bc7e646b5bd5e2b07ce24d781a0568d3a57ff0d
LeonardoYaranga <leonardo.yaranga@gmail.com>
2022-12-12 22:18:11 -0500

    hw09UserDefineExceptions V1.0.0

---
+++ hw/yaranga/u2/hw09UserDefineExceptions/LibrarySimulatorUserDefineExceptions V1.0.0/src/ec/edu/espe/Library/exceptions/IdLessThanOneException.java
@@ -0,0 +1,14 @@
+
+package ec.edu.espe.Library.exceptions;
+
+
+/**
+ *
+ * @author Leonardo Yaranga,Progress Team, DCCO-ESPE
+ */
+public class IdLessThanOneException extends Exception {
+    public IdLessThanOneException(String message){
+    super(message);
+    }
+}
+
