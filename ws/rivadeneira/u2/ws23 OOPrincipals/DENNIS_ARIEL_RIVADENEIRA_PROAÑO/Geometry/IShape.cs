d557af8c91f14b3f27f971c988c7b2e3e66da073
darivadeneira <darivadeneira7@espe.edu.ec>
2022-12-15 08:35:42 -0500

    ws23 OOPrincipals v1.0.0

---
+++ ws/rivadeneira/u2/ws23 OOPrincipals/DENNIS_ARIEL_RIVADENEIRA_PROAÑO/Geometry/IShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    interface IShape
+    {
+        double CalculateArea();
+        double CalculatePerimeter();
+
+    }
+
+}
+
