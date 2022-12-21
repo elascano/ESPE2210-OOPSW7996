ed760db2defadb74ef1b2f955211cc9ac9821074
Lindsay Domenique Barrionuevo Ordoñez <domelindsay@gmail.com>
2022-12-21 08:46:33 -0500

    ws26 SOLID

---
+++ ws/barrionuevo/unit2/ws26/SRP-OCP-LSK-main/SRP-OCP-LSP/01. SRP/02. Wrong/Vehicle.java
@@ -0,0 +1,38 @@
+package srp;
+
+/**
+ *
+ * @author nicko
+ */
+public class Vehicle {
+    private final int maxFuel;
+    private int remainingFuel;
+
+    public Vehicle(final int maxFuel) {
+        this.maxFuel = maxFuel;
+        remainingFuel = maxFuel;
+    }
+
+    //To solve, create a new Class FuelPump
+
+    public void reFuel() {   
+        remainingFuel = maxFuel;
+    }
+
+    public int getMaxFuel() {
+        return maxFuel;
+    }
+
+    public int getRemainingFuel() {
+        return remainingFuel;
+    }
+
+    public void setRemainingFuel(final int remainingFuel) {
+        this.remainingFuel = remainingFuel;
+    }
+
+    public void accelerate() {
+        remainingFuel--;
+    }
+}
+
