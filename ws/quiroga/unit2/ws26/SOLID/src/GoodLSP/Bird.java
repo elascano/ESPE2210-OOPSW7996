7a9f5cc9b9e96b9ff5eb0e40452c48ad60142375
Francisco Quiroga <fsquiroga2@espe.edu.ec>
2022-12-21 09:22:42 -0500

    ws26 SOLID

---
+++ ws/quiroga/unit2/ws26/SOLID/src/GoodLSP/Bird.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GoodLSP;
+
+/**
+ *
+ * @author Francisco Quiroga, Search Engine Bandits , DCCO-ESPE
+ */
+public abstract class Bird {
+    void eat() {
+        System.out.println("Eating");
+    }
+}
