cfb14207523d7246eba607b96103091582a6e645
NicolayChillo <nicolaychillo13@gmail.com>
2022-12-21 09:20:53 -0500

    ws26 v1.0.0

---
+++ ws/chillo/u2/ws26/ExcerciseSOLid/src/ec/edu/espe/ws26/SRP/Ostrich.java
@@ -0,0 +1,19 @@
+package ec.edu.espe.ws26.SRP;
+
+/**
+ *
+ * @author NICO
+ */
+public class Ostrich extends Bird {
+    
+    @Override
+    public void eat() {
+        System.out.println("Ostrich eating");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Ostriches can't fly");
+    }
+    
+}
