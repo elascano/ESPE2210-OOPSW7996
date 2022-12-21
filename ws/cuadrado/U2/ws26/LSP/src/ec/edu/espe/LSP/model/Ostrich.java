d4ae26ee2febdb3b6909ba55e8201a491c78b85e
Jeremy Alejandro Cuadrado Logacho <alejocuadrado1@gmail.com>
2022-12-21 09:18:28 -0500

    ws26

---
+++ ws/cuadrado/U2/ws26/LSP/src/ec/edu/espe/LSP/model/Ostrich.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.LSP.model;
+import ec.edu.espe.LSP.model.Bird;
+/**
+ *
+ * @author Alejandro Cuadrado, DCCO-ESPE
+ */
+class Ostrich extends Bird {
+    @Override
+    void eat() {
+        throw new UnsupportedOperationException();
+    }
+}
