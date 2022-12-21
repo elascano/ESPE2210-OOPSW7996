85c856183ee2532d72144a7a880df880b89f47b4
elkinpabon <elkindres2@gmail.com>
2022-12-21 09:11:55 -0500

    ws26

---
+++ ws/pabon/u2/ws26/03. GOOD LSP/src/lsp/LSP.java
@@ -0,0 +1,19 @@
+package lsp;
+
+/**
+ *
+ * @author Elkin Pabon, GiftSoft TEAM, DCCO-ESPE
+ */
+
+public class LSP {
+    
+    public static void main(String[] args) {
+
+        ostrich Ostrich = new ostrich();
+        Duck duck= new Duck();
+        Ostrich.eat();
+        duck.eat();
+        duck.fly();
+    }
+    
+}
