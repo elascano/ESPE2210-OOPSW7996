e0ab6604419bd87a481c640b46bf15cda678f38a
Juan Pablo Pinza <pinza153@hotmail.com>
2023-01-02 23:34:19 -0500

    HW12_CRUDinMongoDB_Pinza_V1.0.0

---
+++ hw/pinza/Unit 2/hw12/ShopInventory_V2/src/main/java/ec/edu/espe/shopinventory_v2/model/Product.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.shopinventory_v2.model;
+
+/**
+ *
+ * @author Juan Pinza, Search Engine Bandits , DCCO-ESPE
+ */
+public abstract class Product {
+
+    private int id;
+    private String name;
+    private int quantity;
+    private float pricePerUnit;
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + ", quantity=" + quantity + ", pricePerUnit=" + pricePerUnit + '}';
+    }
+
+    public Product() {
+        id = 0;
+        name = "";
+        quantity = 0;
+        pricePerUnit = 0;
+    }
+
+    public Product(int id, String name, int quantity, float pricePerUnit) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public abstract void readProduct();
+
+    public abstract void createProduct();
+
+    public abstract void updateProduct();
+
+    public abstract void deleteProduct();
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the quantity
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the pricePerUnit
+     */
+    public float getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    /**
+     * @param pricePerUnit the pricePerUnit to set
+     */
+    public void setPricePerUnit(float pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+}
