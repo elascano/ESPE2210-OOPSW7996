0f32d789cab9bd3ed8aabd2af20552c3fa7dfcbf
Reishel Dayelin Tipán Ávila <rdtipan1@espe.edu.ec>
2023-01-11 07:22:32 -0500

    v1.0 hw15

---
+++ hw/tipan/u2/hw15/HEMSystem/src/main/java/ec/edu/espe/huertoEcoMarket/model/Product.java
@@ -0,0 +1,70 @@
+package ec.edu.espe.huertoEcoMarket.model;
+
+/**
+ *
+ * @author Reishel Tipan, PACSTORE, DCCO-ESPE
+ */
+public class Product {
+
+    private String id;
+    private String name;
+    private String type;
+
+    public Product(String id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + ", type=" + type + '}';
+    }
+    
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+
+
+}
