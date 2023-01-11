a7a807f2955bc5532d51bbc9786f6456e7af7f3a
Justin <jjvillarroel1@espe.edu.ec>
2023-01-11 00:52:11 -0500

    hw15ConnectionWithMongodb

---
+++ ws/villarroel/u2/ws31/MainStudents/src/ec/edu/espe/grading/model/Student.java
@@ -0,0 +1,83 @@
+
+package ec.edu.espe.grading.model;
+
+/**
+ *
+ * @author Justin Villarroel, ProgressTeam, DCCO-ESPE
+ */
+public class Student {
+    private int id;
+    private String name;
+    private float grades[];
+    private float GPA;
+
+    public Student(int id, String name, float[] grades, float GPA) {
+        this.id = id;
+        this.name = name;
+        this.grades = grades;
+        this.GPA = GPA;
+    }
+
+    
+    
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the GPA
+     */
+    public float getGPA() {
+        return GPA;
+    }
+
+    /**
+     * @param GPA the GPA to set
+     */
+    public void setGPA(float GPA) {
+        this.GPA = GPA;
+    }
+
+    /**
+     * @return the grades
+     */
+    public float[] getGrades() {
+        return grades;
+    }
+
+    /**
+     * @param grades the grades to set
+     */
+    public void setGrades(float[] grades) {
+        this.grades = grades;
+    }
+    
+    
+
+    
+}
