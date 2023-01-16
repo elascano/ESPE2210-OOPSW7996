5fcb3ea27ceee2ae1e558b81d20e47ba7fad3cba
elkinpabon <elkindres2@gmail.com>
2023-01-16 09:30:40 -0500

    Exam 2P

---
+++ exams/pabon/u2/Q71-100/Exam 2P/src/ec/edu/espe/exam/controller/MongoDBConnection.java
@@ -0,0 +1,43 @@
+package ec.edu.espe.exam.controller;
+
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author Elkin Pabon, GiftSoft TEAM, DCCO-ESPE
+ */
+
+public class MongoDBConnection {
+    
+    public static MongoDatabase database = null;
+    
+    public MongoDatabase connectDatabase(){
+        
+        if (database == null) {
+            String uri = "mongodb+srv://eapabon:2002123@database.sdflumv.mongodb.net/test";
+            String db = "Exam2P";
+
+            MongoClient mongoClient = MongoClients.create(uri);
+
+            MongoDBConnection.database = mongoClient.getDatabase(db);
+        }
+        
+        
+        return MongoDBConnection.database;
+    }
+    
+    public com.mongodb.MongoClient conectionMongo(){
+        String uri="mongodb+srv://eapabon:2002123@database.sdflumv.mongodb.net/test";
+        MongoClientURI clientURI;
+        com.mongodb.MongoClient client;
+       
+        clientURI= new MongoClientURI(uri);
+        client = new com.mongodb.MongoClient(clientURI);
+        return client;
+      }
+
+    
+}
