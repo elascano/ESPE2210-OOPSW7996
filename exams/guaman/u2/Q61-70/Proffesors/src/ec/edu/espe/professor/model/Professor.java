219d4863746096bc5e4dc6940b28c9446be2db10
AlexDaniel593 <dalexis203@gmail.com>
2023-01-16 12:36:02 -0500

    Practice and theoretical ExamUnit2

---
+++ exams/guaman/u2/Q61-70/Proffesors/src/ec/edu/espe/professor/model/Professor.java
@@ -0,0 +1,66 @@
+package ec.edu.espe.professor.model;
+
+/**
+ *
+ * @author Alexander Guaman, Developer Bears, DCCO-ESPE
+ */
+public class Professor {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public Professor(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + ", age=" + age + '}';
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
