146c8d1b33c5873c3340de00c45520890dfaff20
JerlyReinoso <jerlyreinoso@gmail.com>
2023-01-16 11:23:04 -0500

    Exam2

---
+++ exams/reinoso/U2/ExamSpeakerReinoso/src/main/java/ec/edu/espe/speaker/model/Speaker.java
@@ -0,0 +1,25 @@
+
+package ec.edu.espe.speaker.model;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import org.bson.Document;
+/**
+ *
+ * @author Jerly Reinoso, Search Engine Bandits, DCCO-ESPE
+ */
+public class Speaker extends Product{
+
+    @Override
+    public Document createProduct() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int readProduct() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+    
+}
