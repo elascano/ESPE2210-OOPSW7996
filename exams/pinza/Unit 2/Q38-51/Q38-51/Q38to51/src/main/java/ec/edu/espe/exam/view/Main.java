757c360c20ba87fd70581cf64caf4e604322dec4
Juan Pablo Pinza <pinza153@hotmail.com>
2023-01-16 08:20:14 -0500

    Theorical Part Q38-51

---
+++ exams/pinza/Unit 2/Q38-51/Q38-51/Q38to51/src/main/java/ec/edu/espe/exam/view/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.exam.view;
+
+/**
+ *
+ * @author Juan Pablo Pinza, Search Engine Bandits, DCCO-ESPE
+ */
+public class Main {
+        public static void main(String[] args) {
+            
+            //All the code application goes here, I'm running out of time :c
+    }
+}
