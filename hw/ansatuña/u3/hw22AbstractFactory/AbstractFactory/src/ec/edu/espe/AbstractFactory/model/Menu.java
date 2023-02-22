c94a65fae5d7fd422d1abb2c0a01aa36d8297c25
Karla Alejandra Ansatuña Andrade <kansatunauemee@gmail.com>
2023-02-21 23:42:03 -0500

    hw22AbstractFactory V1.0.0

---
+++ hw/ansatuña/u3/hw22AbstractFactory/AbstractFactory/src/ec/edu/espe/AbstractFactory/model/Menu.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.AbstractFactory.model;
+
+/**
+ *
+ * @author Karla Ansatuña, Scriptal, DCCO-ESPE
+ */
+public abstract class Menu {
+
+    public String caption;
+
+    public abstract String paint();
+}
