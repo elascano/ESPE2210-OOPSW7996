352236862534ac3ab717016acc0d58b2b298942a
JerlyReinoso <jerlyreinoso@gmail.com>
2023-02-22 00:53:38 -0500

    Hw21,22,23,24

---
+++ hw/reinoso/U3/HW22/hw22AbstractFactory/src/ec/edu/espe/AbstractFactory/model/Button.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.AbstractFactory.model;
+/**
+ *
+ * @author Jerly Reinoso, Search Engine Bandits, DCCO-ESPE
+ */
+public abstract class Button {
+    public String caption;
+    public abstract void paint();
+}
---
+++ hw/reinoso/U3/HW22/hw22AbstractFactory/src/ec/edu/espe/AbstractFactory/model/Menu.java
@@ -0,0 +1,10 @@
+
+package ec.edu.espe.AbstractFactory.model;
+/**
+ *
+ * @author Jerly Reinoso, Search Engine Bandits, DCCO-ESPE
+ */
+public abstract class Menu {
+    public String caption;
+    public abstract void paint();
+}
