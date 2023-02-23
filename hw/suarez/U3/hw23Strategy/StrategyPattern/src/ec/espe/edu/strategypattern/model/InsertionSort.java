9d4ac0268a652897f78c34fede1a4739955cdff4
Matías Suárez Alexander Teran <matiasuarez_4b@hotmail.com>
2023-02-22 22:28:55 -0500

    hw V1.0.0

---
+++ hw/suarez/U3/hw23Strategy/StrategyPattern/src/ec/espe/edu/strategypattern/model/InsertionSort.java
@@ -0,0 +1,25 @@
+package ec.espe.edu.strategypattern.model;
+/**
+ *
+ * @author Matias Suarez,WebMasterTeam,DCCO-ESPE
+ */
+public class InsertionSort implements SortingStrategy {
+
+    @Override
+    public int[] sort(int data[]) {
+        System.out.println("\tInsertion Sort");
+        int n = data.length;
+        for (int i = 1; i < n; i++) {
+            int key = data[i];
+            int j = i - 1;
+
+            while (j >= 0 && data[j] > key) {
+                data[j + 1] = data[j];
+                j--;
+            }
+
+            data[j + 1] = key;
+        }
+        return data;
+    }
+}
