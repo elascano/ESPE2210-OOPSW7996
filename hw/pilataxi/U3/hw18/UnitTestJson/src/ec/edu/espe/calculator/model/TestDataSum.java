8e6543099e4460f12bd2c2f27a41476d4bdb449f
Diego Pilataxi <dapilataxi1@espe.edu.ec>
2023-03-05 23:05:16 -0500

    hwU3

---
+++ hw/pilataxi/U3/hw18/UnitTestJson/src/ec/edu/espe/calculator/model/TestDataSum.java
@@ -0,0 +1,14 @@
+
+package ec.edu.espe.calculator.model;
+
+/**
+ *
+ * @author Diego Pilataxi, GitSoftTeam, DCCO-ESPE
+ */
+public class TestDataSum {
+    
+    public float addend1;
+    public float addend2;
+    public float expectedSum;
+}
+
