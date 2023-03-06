b4a8a47dfbf0bfd706ba237ffe0208e557388c4d
Stephen David Drouet Navarrete <sddrouet@espe.edu.ec>
2023-03-06 12:16:12 -0500

    Exam3P V1.0.0

---
+++ exams/drouet/unit3/StrategyPattern/src/ec/edu/espe/strategy/model/SortingStrategy.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.strategy.model;
+
+/**
+ *
+ * @author Stephen Drouet, Developer Bears, DCC-ESPE
+ */
+public interface SortingStrategy {
+
+    public String sort(int data[]);
+}
