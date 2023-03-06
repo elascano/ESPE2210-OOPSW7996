46bb3c657b10302828e2113cef172bfc04f0d5b5
AllanPanchi <avpanchi@espe.edu.ec>
2023-03-06 08:49:57 -0500

    Exam P3

---
+++ exams/panchi/U3/ExamPooShort/src/ec/edu/espe/controller/SortingStrategy.java
@@ -0,0 +1,12 @@
+
+package ec.edu.espe.controller;
+
+import ec.edu.espe.model.Numbers;
+
+/**
+ *
+ * @author Allan Panchi, GiftSoft Team, DCCO-ESPE
+ */
+public abstract class SortingStrategy {
+    public void sort(Numbers listOfDigits) {};
+}
