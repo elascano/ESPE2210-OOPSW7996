05afd680760dc2ee74be5e9e343574db6f4a9572
Luis Adrián Orozco Ordóñez <laorozco5@espe.edu.ec>
2023-03-06 14:01:04 -0500

    V1.0 Final Exam

---
+++ exams/orozco/u3/FinalExam/src/ec/edu/espe/finalExam/controller/SortingStrategy.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.finalExam.controller;
+
+/**
+ *
+ * @author Luis Orozco, Pythons, DCCO-ESPE
+ */
+public interface SortingStrategy {
+    public void sort(int[] arr);  
+}
+
+
+
