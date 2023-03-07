d58f2fe0881b26fcb1aafc4f6dbe80dd78fad7e0
Martín Suquillo <mmsuquillo@espe.edu.ec>
2023-03-07 08:27:56 -0500

    Exam v1.0.1

---
+++ exams/suquillo/u3/ExamU3/OPP Final Exam/SortAppExercise/src/ec/edu/espe/sort/model/SortingContext.java
@@ -0,0 +1,31 @@
+package ec.edu.espe.sort.model;
+/**
+ *
+ * @author Martín Suquillo,WebMasterTeam, DCCO-ESPE
+ */
+public class SortingContext {
+    private SortingStrategy ss;
+    
+    public int[] sort(int data []){
+        int size = data.length;
+        ss = setSortStrategy(size);
+        return ss.sort(data);
+    }
+    public SortingStrategy setSortStrategy(int n){
+        if(n>0 && n<=3){
+            System.out.println("-> Apply Bubble Sort");
+            ss = new BubbleSort();
+        }
+        if(n>4 && n<=7){
+            System.out.println("-> Apply Insertion Sort");
+            ss = new InsertionSort();
+        }
+        if(n> 7){
+            System.out.println("-> Apply Quick Sort");
+            ss = new QuickSort();
+        }
+        return ss;
+    }
+
+    
+}
