ffd145b9254fcf0decd2853fa11fee95e6b78c80
CARLOS ALEXANDER TORRES PINCAY <carlos2002torres70@gmail.com>
2023-03-07 10:24:11 -0500

    V2.0.0 UsCalculatorSingleton

--- hw/torres/u3/Hw20/USCalculator/src/ec/edu/espe/uscalculator/view/Calculator.java
+++ hw/torres/u3/Hw20/USCalculator/src/ec/edu/espe/uscalculator/view/Calculator.java
@@ -18,9 +18,24 @@ public class Calculator {
         tax.salesTotal(price);
         
                 
+        Scanner console = new Scanner(System.in);
+        USTax tax = USTax.getInstance();
+        System.out.println("Imput price of the product:");
+        price = Float.parseFloat(console.nextLine());
+        
+        total = tax.salesTotal(price);
+        
         System.out.println("Price: $" + price);
-        System.out.println("Tax: $ " +  +"%");
-        System.out.println("Sales total: " + USTax.salesTotal());
+        System.out.println("Tax: $" +tax.getTax() + " %" );
+        System.out.println("Sales total: " + total);
+        
+        tax.modifyTax(8.0F);
+        
+        total = tax.salesTotal(price);
+        
+        System.out.println("Price: $" + price);
+        System.out.println("Tax: $" +tax.getTax() + " %" );
+        System.out.println("Sales total: " + total);
     }
     
     
